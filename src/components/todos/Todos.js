397f2078fd88aa6c886d0efec74d316a096ce98f
DorfStil <dorfstil@gmail.com>
2023-12-27 14:17:02 +0400

    add select and send buttons

--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -9,24 +9,28 @@ export default function Todos({ className, id }) {
   return (
     <section className={className ?? 'todos'} id={id}>
 
-      <header className="todos__header">
-        <Heading className='todos__heading heading'>Todos</Heading>
-        <Input className="todos__input input input--text" type="text" placeholder="Что необходимо сделать?" />
-      </header>
+        <header className="todos__header">
+          <Heading className='todos__heading heading'>Todos</Heading>
+          <div className="todos__input-wrapper">
+            <Button className='todos__select-button button--select' />
+            <Input className="todos__input input input--text" type="text" placeholder="Что сделаем?" />
+            <Button className='todos__send-button button--send' type='submit' id='todoSend' />
+          </div>
+        </header>
 
-      <section className="todos__main">
-        <ul className="todos__list">
-          <TodoItem>Сходить за хлебом</TodoItem>
-          <TodoItem>Провести генеральную уборку</TodoItem>
-          <TodoItem>Позвонить родственникам</TodoItem>
-        </ul>
-      </section>
+        <section className="todos__main">
+          <ul className="todos__list">
+            <TodoItem>Сходить за хлебом</TodoItem>
+            <TodoItem>Провести генеральную уборку</TodoItem>
+            <TodoItem>Позвонить родственникам</TodoItem>
+          </ul>
+        </section>
 
-      <footer className="todos__footer">
-        <span className="todos__counter">Задач осталось 4</span>
-        <Filter className='todos__filters filter filter--horizontal' />
-        <Button className="todos__button button--link active">Очистить</Button>
-      </footer>
+        <footer className="todos__footer">
+          <span className="todos__counter">Задач осталось 4</span>
+          <Filter className='filter filter--horizontal' />
+          <Button className="todos__button button--link active">Очистить</Button>
+        </footer>
 
     </section>
   )

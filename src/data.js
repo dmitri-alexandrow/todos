11fd51164c554f687e6bc3b2af90eb93bd2f3239
DorfStil <dorfstil@gmail.com>
2023-12-29 16:31:50 +0400

    clear code

--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
 import { readFromLocalStorage } from "./utils";
 
 export const STORE = readFromLocalStorage('taskList');
+export const INITIAL_STORE = [{
+  id: 1,
+  text: 'Мой первый таск',
+  isCompleted: false
+}];
